5a2006b4a987484b421f5f7cc527e10bbb544dc1
Wai H Leung <snail0830@gmail.com>
2020-02-09 18:42:12 -0800

    secound commit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ document.getElementById("button").addEventListener("click", () => {
     image1.setAttribute("src", "images/dice" + num1 + ".png");
     image2.setAttribute("src", "images/dice" + num2 + ".png");
     if (num1 > num2){
-        document.querySelector("h1").innerHTML = "Player 1 Win!!";
+        document.querySelector("h1").innerHTML = "Player 1 Win!";
     }
     else if (num2 > num1){
-        document.querySelector("h1").innerHTML = "Player 2 Win!!";
+        document.querySelector("h1").innerHTML = "Player 2 Win!";
     }
     else {
         document.querySelector("h1").innerHTML = "Draw!!";
